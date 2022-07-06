44980649ebce50733adbbc54956731795369ab96
Puja Kusuma <pujakusumae@hotmail.com>
2022-07-06 15:13:49 +0700

    Disable SPI DMA

--- driver/v3/ae210p/config/RTE_Device.h
+++ driver/v3/ae210p/config/RTE_Device.h
@@ -87,13 +87,13 @@
 // DMA
 //   Tx
 //     Channel     <0=>0 <1=>1 <2=>2 <3=>3 <4=>4 <5=>5 <6=>6 <7=>7
-#define RTE_SPI1_DMA_TX_EN              1
+#define RTE_SPI1_DMA_TX_EN              0
 #define RTE_SPI1_DMA_TX_CH              0
 #define RTE_SPI1_DMA_TX_REQID           2
 
 //   Rx
 //     Channel     <0=>0 <1=>1 <2=>2 <3=>3 <4=>4 <5=>5 <6=>6 <7=>7
-#define RTE_SPI1_DMA_RX_EN              1
+#define RTE_SPI1_DMA_RX_EN              0
 #define RTE_SPI1_DMA_RX_CH              1
 #define RTE_SPI1_DMA_RX_REQID           3
 
